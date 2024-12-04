e5d8e351416fc45c95953307b7e6325c1c69dd3a
tro3373 <tro3373@gmail.com>
2024-12-04 09:42:26 +0900

    fix: Fix error on vercel dev or error in vercel server cause listen 3000 port

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-// import type { VercelRequest, VercelResponse } from '@vercel/node';
-// export default function handler(req: VercelRequest, res: VercelResponse) {
-//   const { name = 'World' } = req.query;
-//   return res.json({
-//     message: `Hello ${name}!`,
-//   });
-// }
-
 import express from 'express';
 import type { Request, Response } from 'express';
 
 const app = express();
-// const port = process.env.PORT || 3000;
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,8 +12,10 @@ app.get('/', (_req: Request, res: Response) => {
 app.get('/ping', (_req: Request, res: Response) => {
   res.send({ msg: 'pong' });
 });
-app.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-});
+if (process.env.START_LISTEN === '1') {
+  app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+  });
+}
 // biome-ignore lint/style/noDefaultExport: ignore
 export default app;
